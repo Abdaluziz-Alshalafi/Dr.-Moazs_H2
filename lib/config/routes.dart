15579796b7a5f57e6c292afe09ea3ce548a2975a
Abdaluziz-Alshalafi <com775527735@gmail.com>
2024-12-22 15:29:41 -0800

    Update routes.dart

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -5,8 +5,5 @@ const String baseRoute = '/';
 const String homeRoute = '/home';
 
 const String loginRoute = '/login';
-// Map<String,WidgetBuilder>app
 
-const String home1Route = '/home1';
-const String providerRoute = '/provider';
-const String getXRoute = '/getx';
+
