d2eac5559a2afc07a292956ce2a669b2466d1f32
Abdaluziz-Alshalafi <com775527735@gmail.com>
2024-12-22 15:23:05 -0800

    Update home_page.dart

--- lib/Views/home_page.dart
+++ lib/Views/home_page.dart
@@ -1,12 +1,12 @@
 // import 'package:test_2/widgets/Abdaluziz_button.dart';
-// import 'package:abdaluziz_hw_1/Views/CustomAppBar.dart';
-import 'package:abdaluziz_hw_1/Views/mode_icon.dart';
+import 'package:abdaluziz_hw_1/Views/CustomAppBar.dart';
+import 'package:abdaluziz_hw_1/control/theme_control.dart';
 import 'package:abdaluziz_hw_1/theme/styles.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:abdaluziz_hw_1/theme/color.dart';
 import 'package:abdaluziz_hw_1/views/login_page.dart';
 import 'package:abdaluziz_hw_1/widgets/Abdaluziz_button.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,52 +15,38 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final Themestyles themeStyles = Themestyles();
+    final Theme_control theme_control = Theme_control();
+
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: themeStyles.LightTheme,
+      darkTheme: themeStyles.darkTheme,
+      themeMode: theme_control.getThemeMode(),
+      home: HomePage(),
+    );
+  }
+}
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    const moonIcon = CupertinoIcons.moon_stars;
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          const SizedBox(width: 20),
-          IconButton(
-            icon: const Icon(
-              moonIcon,
-              color: Colors.grey,
-            ),
-            onPressed: () {
-              Themestyles().changeTheme();
+      appBar: const CustomAppBar(),
+      body: Center(
+        child: AbdaluzizButton(
+            voidCallback: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
             },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          AbdaluzizButton(
-              voidCallback: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              name: "project",
-              shadowColor: AppColor.secandryColor),
-          SizedBox(
-            height: 10,
-          ),
-          AbdaluzizButton(
-              voidCallback: () {
-                Themestyles().darkTheme;
-              },
-              name: "تسجيل دخول",
-              shadowColor: Colors.red),
-          SizedBox(
-            height: 10,
-          ),
-        ],
+            name: "تسجيل دخول",
+            shadowColor: Colors.red),
       ),
     );
   }
