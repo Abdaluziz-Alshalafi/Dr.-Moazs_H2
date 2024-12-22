5cbe7520139d727849a950bc786190bc541bf6b9
Abdaluziz-Alshalafi <com775527735@gmail.com>
2024-12-22 15:24:52 -0800

    Update styles.dart

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:abdaluziz_hw_1/theme/color.dart';
 
 class Themestyles {
   final LightTheme = ThemeData.light().copyWith(
@@ -16,25 +17,4 @@ class Themestyles {
     scaffoldBackgroundColor: Colors.blueGrey.shade900,
     dividerColor: Colors.white54,
   );
-
-  final _getStorage = GetStorage();
-  final _darkTwhemeKey = 'isDarkTheme';
-
-  void saveThemeData(bool isDarkTheme) {
-    _getStorage.write(_darkTwhemeKey, isDarkTheme);
-  }
-
-  bool isSavedDarkMode() {
-    // return _getStorage.read(_darkTwhemeKey) ?? false;
-    return true;
-  }
-
-  ThemeMode getThemeMode() {
-    return isSavedDarkMode() ? ThemeMode.dark : ThemeMode.light;
-  }
-
-  void changeTheme() {
-    Get.changeThemeMode(isSavedDarkMode() ? ThemeMode.light : ThemeMode.dark);
-    saveThemeData(!isSavedDarkMode());
-  }
 }
