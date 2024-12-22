a011110491641de3511680c8db4894e6a4f61470
Abdaluziz-Alshalafi <com775527735@gmail.com>
2024-12-22 15:24:34 -0800

    Update color.dart

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,6 +1,8 @@
+import 'package:abdaluziz_hw_1/control/theme_control.dart';
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Colors.blue;
+Color appBar_color = Color.fromARGB(225, 88, 137, 170);
 
 class AppColor {
   static const Color secandryColor = Colors.grey;
